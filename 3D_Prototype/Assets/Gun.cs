8745317141c6c2d8206f69e28293c750c4455382
DominicFoulem <dfoulem@comcast.net>
2022-05-18 10:53:43 -0600

    update

---
+++ 3D_Prototype/Assets/Gun.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    bool attacked = false;
+    float timer = 0;
+    public Transform firePoint;
+    public GameObject FireballPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (attacked == true)
+        {
+            timer += Time.deltaTime;
+        }
+        if (timer >= 0.5f)
+        {
+            attacked = false;
+            timer = 0;
+        }
+        if (Input.GetButtonDown("Fire1") && attacked == false)
+        {
+            Shoot();
+            attacked = true;
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(FireballPrefab, firePoint.position, firePoint.rotation);
+    }
+
+}
